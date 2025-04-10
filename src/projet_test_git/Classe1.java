b0d9f876c881bce1ffec71be3f9d42517279c838
Amal <amalhallou@gmail.com>
2025-04-10 11:26:26 +0100

    fichier classes

---
+++ src/projet_test_git/Classe1.java
@@ -0,0 +1,11 @@
+package projet_test_git;
+
+public class Classe1 {
+	
+	public static void main(String[] args) {
+			System.out.println("Classe de test 1");
+			Classe2 objet2=new Classe2();
+			objet2.afficher();
+	}
+
+}
---
+++ src/projet_test_git/Classe2.java
@@ -0,0 +1,16 @@
+package projet_test_git;
+
+public class Classe2 {
+	String nom;
+	Classe2(){
+		nom="test";
+	}
+	Classe2(String nom){
+		this.nom=nom;
+	}
+	public void afficher(){
+		System.out.println("classe de test "+nom);
+		
+	}
+
+}
